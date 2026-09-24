e3b1584c614f096a4d1ec4e146050f6d597bcfc4
Abhishek shaw <150148003+shawAbhishek010@users.noreply.github.com>
2026-09-24 22:45:18 +0530

    Time: 18 ms (14.42%), Space: 47.9 MB (52.13%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,57 @@
+public class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null)
+            return "";
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (sb.length() > 0)
+                sb.append(",");
+            if (node == null) {
+                sb.append("n");
+            } 
+            else {
+                sb.append(node.val);
+                q.offer(node.left);
+                q.offer(node.right);
+            }
+        }
+        return sb.toString();
+    }
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == null || data.isEmpty())
+            return null;
+        String[] parts = data.split(",");
+        if (parts[0].equals("n"))
+            return null;
+        TreeNode root = new TreeNode(Integer.parseInt(parts[0]));
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int i = 1;
+        while (!q.isEmpty() && i < parts.length) {
+            TreeNode parent = q.poll();
+            // Left child
+            if (!parts[i].equals("n")) {
+                TreeNode left = new TreeNode(Integer.parseInt(parts[i]));
+                parent.left = left;
+                q.offer(left);
+            }
+            i++;
+
+            // Right child
+            if (i < parts.length && !parts[i].equals("n")) {
+                TreeNode right = new TreeNode(Integer.parseInt(parts[i]));
+                parent.right = right;
+                q.offer(right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+}
